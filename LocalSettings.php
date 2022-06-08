5d8d637760cf30ba148a77dfbb23ba6ef9bc934f
Magus Wazir <maguswazir@magicmachine.co>
2022-06-08 15:26:04 -0500

    added script path

--- LocalSettings.php
+++ LocalSettings.php
@@ -29,7 +29,7 @@ $wgShowExceptionDetails = true;
 ## For more information on customizing the URLs
 ## (like /w/index.php/Page_title to /wiki/Page_title) please see:
 ## https://www.mediawiki.org/wiki/Manual:Short_URL
-$wgScriptPath = "";
+$wgScriptPath = "w";
 
 ## The protocol and server name to use in fully-qualified URLs
 $wgServer = "https://wizzypedia.herokuapp.com";
